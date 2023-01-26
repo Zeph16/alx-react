9a854dc89f0d5699c9f79058dd33a4bc1efd0818
Sofonias Ayele <sof16ayele@gmail.com>
2023-01-26 22:36:01 +0300

    Webpack task 0

---
+++ 0x00-Webpack/task_0/src/index.js
@@ -0,0 +1,7 @@
+import $ from 'jquery';
+
+var p1 = "<p>Holberton Dashboard</p>";
+var p2 = "<p>Dashboard data for the student</p>";
+var p3 = "<p>Copyright - Holberton School";
+
+$('body').append(p1, p2, p3);
