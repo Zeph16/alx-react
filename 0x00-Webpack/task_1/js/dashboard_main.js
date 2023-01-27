d957b29da012e1d3dcf42b2a658a0a9bd43bdebd
Sofonias Ayele <sof16ayele@gmail.com>
2023-01-27 03:37:23 +0300

    Task 1

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -8,8 +8,10 @@ $('body').append('<p id="count"></p>');
 $('body').append('<p>Copyright - Holberton School');
 
 let count = 0;
+
 function updateCounter() {
   count++;
   $('#count').html(`${count} clicks on the button`);
 };
-$('button').on('click', _.debounce(updateCounter, 300));
+
+$('button').on('click', _.debounce(updateCounter, 500));
