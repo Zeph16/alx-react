3adaa5ebc1ee69bfada54624df258d03934c9d3a
Sofonias Ayele <sof16ayele@gmail.com>
2023-01-27 03:24:21 +0300

    Task 1

---
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,15 @@
+import $ from 'jquery';
+import _ from 'lodash';
+
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</p>');
+$('body').append('<p id="count"></p>');
+$('body').append('<p>Copyright - Holberton School');
+
+var count = 0;
+function updateCounter() {
+  count++;
+  $('#count').html(`${count} clicks on the button`);
+};
+$('button').on('click', _.debounce(updateCounter, 300));
